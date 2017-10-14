3b938fcf8c0ad29ba60372048deccee4e390d8dd
Brian Kramer <subsoap@gmail.com>
2017-10-14 00:52:57 -0700

    Model with sprites attached prefab test

---
+++ defold-standard-geometry/gos/ship.go
@@ -0,0 +1,15 @@
+components {
+  id: "ship"
+  component: "/defold-standard-geometry/models/ship.model"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
